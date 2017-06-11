fc61843b19d7b5d98ca4bbcd4f4c0bef4af3eef9
rswby <s15965213275@163.com>
2017-06-12 04:14:53 +0800

    删除重复

--- study170611.js
+++
@@ -1,7 +0,0 @@
-"通过new http.Server()创建的服务器是通过监听request请求来回应数据的,记成监听data了记错了!";
-var server = new http.Server();
-server.on('request', (req, res) => {
-    res.end('hello world');
-    console.log('hello world');
-});
-"在想解决异步回调的方法了";
