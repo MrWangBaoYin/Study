f6ab8b0b3eb0b189ce022a77a8f477ad63bd36e4
rswby <s15965213275@163.com>
2017-06-14 05:52:23 +0800

    修改一下,好像声明太多变量会影响性能

--- diary/认证的接收.js
+++ diary/认证的接收.js
@@ -4,12 +4,10 @@ http.createServer((req, res) => {
     //console.log(req);
     //res.end('先寻找下验证');
     /*认证在req.headers.authorization*/
-    var auth = req.headers.authorization;
-    console.log(auth);
-    var authCon = auth.split(' ')[1];
-    var parsedAC = new Buffer(authCon, 'base64').toString('utf8');
-    console.log(parsedAC);
-    var aparsedAc = parsedAC.split(':');
+    var authCon = req.headers.authorization.split(' ')[1]; //Basic ...... 中间有一个空格,不知道Basic是干什么的
+    var aparsedAc = new Buffer(authCon, 'base64').toString('utf8').split(':');
+    //网上找的,不知道原理
+    //console.log(aparsedAC);
     var user = aparsedAc[0];
     var pwd = aparsedAc[1];
     if (user === 'rswby' && pwd === 'nihaoa') {
