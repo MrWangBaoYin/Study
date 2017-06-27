b69ebd18cdeaaadf5efc87e832f6ea6c9c09c3c2
rswby <s15965213275@163.com>
2017-06-28 03:05:45 +0800

    learnyounode complete start how-to-npm

---
+++ how-to-npm/README.md
@@ -0,0 +1,1 @@
+this is a how-to-npm exercise module!
--- learnyounode/asyncFS.js
+++ learnyounode/asyncFS.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-
+//在使用字符编码后data变为字符串
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
     if (err) {
         console.error(err);
